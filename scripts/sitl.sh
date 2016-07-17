83fc6015023aabd68dc083ebb1ac29c20c7db8f8
James Goppert <james.goppert@gmail.com>
2016-07-17 11:13:22 -0400

    Added integration testing support and estimator selection.

--- scripts/sitl.sh
+++ scripts/sitl.sh
@@ -6,6 +6,7 @@ set -x
 rc_script=$1
 model=$2
 data_path=$3
+est=$4
 program="gazebo"
 
 echo PX4 ARGS
@@ -37,11 +38,24 @@ fi
 
 if [ "$#" -lt 3 ]
 then
-	echo usage: $0 rc_script model data_path
+	echo usage: $0 rc_script model data_path estimator
 	echo ""
+	exit 1
+fi
+
+est_string=""
+
+if [ "$est" == "ekf2" ]
+then
+	est_string=""
+elif [ "$est" == "lpe" ]
+then
+	est_string="_lpe"
+else
+	echo unknown estimator $est
 	exit 1
 fi
 
 # Do not exit on failure now from here on because we want the complete cleanup
 set +e
-cd $data_path && $sudo_enabled px4 $chroot_enabled ${rc_script}_${program}_${model}
+cd $data_path && $sudo_enabled px4 $chroot_enabled ${rc_script}${est_string}_${program}_${model}
